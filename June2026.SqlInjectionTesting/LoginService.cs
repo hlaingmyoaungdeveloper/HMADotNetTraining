452a667bec9d9346c96a4553ed08d84ab809d229
hlaingmyoaungdeveloper <aungdeveloperhlaingmyo@gmail.com>
2026-07-14 11:58:33 +0630

    feat:prevent SQL injection

---
+++ June2026.SqlInjectionTesting/LoginService.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June2026.SqlInjectionTesting
+{
+    internal class LoginService
+    {
+        private readonly SqlConnectionStringBuilder _sb;
+
+        public LoginService()
+        {
+            _sb = new SqlConnectionStringBuilder()
+            {
+                DataSource = "ASPIERLITE16",
+                InitialCatalog = "June2026Db",
+                UserID = "sa",
+                Password = "sasa@123",
+                TrustServerCertificate = true
+            };
+        }
+
+        public void Login(string username, string password)
+        {
+            using(IDbConnection db = new SqlConnection(_sb.ConnectionString))
+            {
+                var user = db.Query("Select * from Tbl_User Where Username=@username and Password=@password", new
+                {
+                    Username = username,
+                    Password = password
+                }).FirstOrDefault();
+                //var user = db.Query("Select * from Tbl_User Where Username=username and Password=password"); this can do sqlinjection
+                if (user != null)
+                {
+                    Console.WriteLine("Login successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Login Fail");
+                }
+            }
+        }
+    }
+}
---
+++ June2026.SqlInjectionTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using June2026.SqlInjectionTesting;
+
+Console.WriteLine("Hello, World!");
+LoginService service = new LoginService();
+Console.Write("Enter the username :");
+string username = Console.ReadLine()!;
+Console.Write("\nEnter the password:");
+string password = Console.ReadLine()!;
+service.Login(username, password);
