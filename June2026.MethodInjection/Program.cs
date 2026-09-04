e61fd225e92e3258b8b584ad616801af18f3ad2d
hlaingmyoaungdeveloper <aungdeveloperhlaingmyo@gmail.com>
2026-09-04 23:28:33 +0630

    feat: method injection testing

---
+++ June2026.MethodInjection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
+
+public class OrderProcessor
+{
+
+    public void ProcessOrder(int orderId, ILogger logger)
+    {
+        Console.WriteLine($"Processing Order: {orderId}");
+
+        logger.Log($"Order {orderId} has been processed successfully.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        OrderProcessor processor = new OrderProcessor();
+        ILogger myLogger = new ConsoleLogger();
+
+        processor.ProcessOrder(101, myLogger);
+    }
+}
+//use [FromServices] in ASP.NET Core Controller
