23bf8808396ff6b9fab10908fd1e1ecf9ce6b9d1
hlaingmyoaungdeveloper <aungdeveloperhlaingmyo@gmail.com>
2026-09-04 23:45:44 +0630

    feat:property injection testing

---
+++ June2026.PropertyInjection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
+
+public class CustomerService
+{
+    public ILogger Logger { get; set; }
+
+    public void CreateCustomer(string name)
+    {
+        Console.WriteLine($"Customer '{name}' created.");
+
+   
+        if (Logger != null)
+        {
+            Logger.Log($"Customer {name} was successfully saved.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CustomerService service = new CustomerService();
+
+        service.Logger = new ConsoleLogger();
+
+        service.CreateCustomer("Aung Aung");
+    }
+}
