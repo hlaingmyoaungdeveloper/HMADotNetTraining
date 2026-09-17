420174c1e76324565c0d1b251ada128b24ca8691
hlaingmyoaungdeveloper <aungdeveloperhlaingmyo@gmail.com>
2026-09-17 12:21:35 +0630

    feat : implement middleware

---
+++ June2026.MiddlewareWebApp/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace June2026.MiddlewareWebApp.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(LoginRequestModel requestModel)
+    {
+        Response.Cookies.Append("Username", requestModel.Username);
+        return Redirect("/home");
+    }
+}
+
+public class LoginRequestModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ June2026.MiddlewareWebApp/Middlewares/CookieMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace June2026.MiddlewareWebApp.Middlewares;
+
+public class CookieMiddleware
+{
+    public readonly RequestDelegate _next;
+
+    public CookieMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path.ToString().ToLower() != "/login" &&
+           context.Request.Path.ToString().ToLower() != "/login/Index")
+        {
+            if (!context.Request.Cookies.Any(x => x.Key == "Username"))
+            {
+                context.Response.Redirect("/login");
+            }
+        }
+        await _next(context);
+
+    }
+}
+
+public static class CookieMiddlewareExtension
+{
+    public static void UseCookieMiddleware(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<CookieMiddleware>();
+    }
+}
