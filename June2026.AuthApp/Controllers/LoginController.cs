3833451b2f8b428a0ac3c20a1452349f41a6f2e5
hlaingmyoaungdeveloper <aungdeveloperhlaingmyo@gmail.com>
2026-09-22 21:10:44 +0630

    feat : implement authentication

---
+++ June2026.AuthApp/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace June2026.MiddlewareWebApp.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> IndexAsync(LoginRequestModel requestModel)
+    {
+        var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, requestModel.Username),
+                new Claim(ClaimTypes.Role, requestModel.Username),
+                new Claim("LastLogin", DateTime.UtcNow.ToString())
+            };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            IsPersistent = requestModel.RememberMe, // "Remember me" option
+            ExpiresUtc = DateTimeOffset.UtcNow.AddHours(8)
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties);
+        return Redirect("/home");
+    }
+}
+
+public class LoginRequestModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
